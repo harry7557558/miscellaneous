b3936615dbb355b66c656b088ed579e41824a375
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2020-03-06 04:17:41 -0500

    Update Polynomial Regression.cpp

--- Polynomial Regression.cpp
+++ Polynomial Regression.cpp
@@ -1,48 +1,46 @@
+// Polynomial Regression
+// Designed to fit functions that are expensive to calculate
+// It is not recommand to fit a polynomial with degree higher than 6 since they often have high computing error
+
+
 #include <cmath>
 #include <stdio.h>
 
 #define PI 3.1415926535897932384626
 
 
-
-
-// highest degree term comes first (where the highest term is c[0] and constant term is c[n])
-double evalPolynomial(const double *c, int N, double t) {
-	double r = c[0];
-	for (int i = 1; i <= N; i++) r = r * t + c[i];
+// highest degree term comes first (where the highest term is P[0] and constant term is P[n])
+double evalPolynomial(const double *P, int N, double x) {
+	double r = P[0];
+	for (int i = 1; i <= N; i++) r = r * x + P[i];
 	return r;
+}
+
+// Gaussian elimination for general situation
+void Eliminate(double* Mat, double *X, int N) {
+	for (int i = 0; i < N; i++) for (int j = i + 1; j < N; j++) {
+		double d = Mat[j*N + i] / Mat[i*N + i];
+		for (int k = i; k < N; k++) Mat[j*N + k] -= Mat[i*N + k] * d;
+		X[j] -= X[i] * d;
+	}
+	for (int i = N - 1; i >= 0; i--) for (int j = i - 1; j >= 0; j--) {
+		double d = Mat[j*N + i] / Mat[i*N + i];
+		for (int k = N - 1; k >= i; k--) Mat[j*N + k] -= Mat[i*N + k] * d;
+		X[j] -= X[i] * d;
+	}
+	for (int i = 0; i < N; i++) X[i] /= Mat[i*N + i];
 }
 
 
 
-// Polynomial Regression
-// It is not recommand to fit a polynomial with degree higher than 8 since they often have high computing error
 
 #pragma region Polynomial Fitting Macros
-
-// Try to study the property of the matrix and find a better way to solve
-// M[i][j] = 1/k (bᵏ-aᵏ), k = 2N-(i+j)+1, zero-indexed
-// M[i][j] = ∑ᵢ xᵢ^(2N-i-j)
 
 #define InitMat \
 	double *Mat = new double[L*L]; \
 	for (int i = 0; i < L; i++) for (int j = 0; j < L; j++) Mat[i*L + j] = sumxn[2 * N - i - j]; \
-	for (int i = 0; i < L; i++) c[i] = sumxny[N - i]; \
+	for (int i = 0; i < L; i++) P[i] = sumxny[N - i]; \
 	delete sumxn, sumxny;
-
-#define Eliminate \
-	for (int i = 0; i < L; i++) for (int j = i + 1; j < L; j++) { \
-		double d = Mat[j*L + i] / Mat[i*L + i]; \
-		for (int k = i; k < L; k++) Mat[j*L + k] -= Mat[i*L + k] * d; \
-		c[j] -= c[i] * d; \
-	} \
-	for (int i = L - 1; i >= 0; i--) for (int j = i - 1; j >= 0; j--) { \
-		double d = Mat[j*L + i] / Mat[i*L + i]; \
-		for (int k = L - 1; k >= i; k--) Mat[j*L + k] -= Mat[i*L + k] * d; \
-		c[j] -= c[i] * d; \
-	} \
-	for (int i = 0; i < L; i++) c[i] /= Mat[i*L + i]; \
-	delete Mat;
 
 #define CalcError(x_i_, dif_x_) \
 	double E = 0, MaxE = 0; \
@@ -57,8 +55,9 @@ double evalPolynomial(const double *c, int N, double t) {
 
 #pragma endregion Repeated part of fitPolynomial functions
 
+
 // fit to data set
-void fitPolynomial(double *x, double *y, int n, double *c, int N) {
+void fitPolynomial(double *x, double *y, int n, double *P, int N) {
 	int L = N + 1;
 	double *sumxn = new double[2 * N + 1], *sumxny = new double[L];
 	for (int i = 0; i <= 2 * N; i++) sumxn[i] = 0;
@@ -69,86 +68,87 @@ void fitPolynomial(double *x, double *y, int n, double *c, int N) {
 		for (j = 0, t = 1.0; j <= N; j++) sumxny[j] += t * y[i], t *= x[i];
 	}
 	InitMat;
-	Eliminate;
+	Eliminate(Mat, P, L);
+	delete Mat;
 }
 
-// fit a function in interval [x0,x1], where dif is the sample in numerical integration
-void fitPolynomial(double(*f)(double), double x0, double x1, int dif, double *c, int N, double *RMSE, double *ERR) {
-	int L = N + 1;
-	double *sumxn = new double[2 * N + 1], *sumxny = new double[L];  // or intxn, intxny
-	for (int d = 0; d <= 2 * N; d++) sumxn[d] = (pow(x1, d + 1) - pow(x0, d + 1)) / (d + 1);
-
-	// numerical integration
-	int n = dif * 2;
+// fit a function in interval [x0,x1], where dif is the number of samples in numerical integration
+void fitPolynomial(double(*f)(double), double x0, double x1, int dif, double *P, int N, double *RMSE, double *ERR) {
+	int L = N + 1, n = dif * 2;
 	double u = (x1 - x0) / n, x, xp;
-	for (int d = 0; d < L; d++) sumxny[d] = 0;
-	for (int i = 0; i <= n; i++) {
-		x = x0 + u * i, xp = i == 0 || i == n ? 1.0 : i & 1 ? 4.0 : 2.0;
-		for (int d = 0; d < L; d++) sumxny[d] += xp * f(x), xp *= x;
+	double *sumxn = new double[2 * N + 1], *sumxny = new double[L];  // or intxn, intxny
+
+	// sumxn[i] = Integral[xⁱ,x0,x1]
+	for (int i = 0; i <= 2 * N; i++) sumxn[i] = (pow(x1, i + 1) - pow(x0, i + 1)) / (i + 1);
+
+	// sumxny[i] = Integral[xⁱ·f(x),x0,x1]
+	for (int i = 0; i < L; i++) sumxny[i] = 0;
+	for (int d = 0; d <= n; d++) {
+		x = x0 + u * d, xp = (d == 0 || d == n ? 1.0 : d & 1 ? 4.0 : 2.0) * f(x);
+		for (int i = 0; i < L; i++) sumxny[i] += xp, xp *= x;
 	}
-	for (int d = 0; d < L; d++) sumxny[d] *= u / 3;
+	for (int i = 0; i < L; i++) sumxny[i] *= u / 3;
 
 	InitMat;
-	Eliminate;
+	Eliminate(Mat, P, L); delete Mat;
 
 	// calculate error - spend time!
-	if (RMSE || ERR) { CalcError(x0 + u * i, evalPolynomial(c, N, x) - f(x)); }
+	if (RMSE || ERR) { CalcError(x0 + u * i, evalPolynomial(P, N, x) - f(x)); }
 }
 
-// fit a function in interval [0,x1], force the constant term of the polynomial to be 0
-void fitPolynomial(double(*f)(double), double x1, int dif, double *c, int N, double *RMSE, double *ERR) {
-	// much copied from the previous function
+// fit a function in interval [0,x1] where P(0)=0
+void fitPolynomial(double(*f)(double), double x1, int dif, double *P, int N, double *RMSE, double *ERR) {
+	// same as the previous function
 	int L = N + 1, n = dif * 2;
-	double *sumxn = new double[2 * N + 1], *sumxny = new double[L];
-	for (int d = 0; d <= 2 * N; d++) sumxn[d] = pow(x1, d + 1) / (d + 1);
 	double u = x1 / n, x, xp;
-	for (int d = 0; d < L; d++) sumxny[d] = 0;
-	for (int i = 0; i <= n; i++) {
-		x = u * i, xp = i == 0 || i == n ? 1.0 : i & 1 ? 4.0 : 2.0;
-		for (int d = 0; d < L; d++) sumxny[d] += xp * f(x), xp *= x;
+	double *sumxn = new double[2 * N + 1], *sumxny = new double[L];
+	for (int i = 0; i <= 2 * N; i++) sumxn[i] = pow(x1, i + 1) / (i + 1);
+	for (int i = 0; i < L; i++) sumxny[i] = 0;
+	for (int d = 0; d <= n; d++) {
+		x = u * d, xp = (d == 0 || d == n ? 1.0 : d & 1 ? 4.0 : 2.0) * f(x);
+		for (int i = 0; i < L; i++) sumxny[i] += xp, xp *= x;
 	}
-	for (int d = 0; d < L; d++) sumxny[d] *= u / 3;
+	for (int i = 0; i < L; i++) sumxny[i] *= u / 3;
 
 	// a lazy approach, only this part is different from the previous function
 	InitMat;
 	for (int i = 0; i < N; i++) Mat[i*L + N] = Mat[N*L + i] = 0;
-	c[N] = 1;
-	Eliminate;
-	c[N] = 0;
+	P[N] = 1;
+	Eliminate(Mat, P, L); delete Mat;
+	P[N] = 0;
 
-	// calculate error - spend time!
-	if (RMSE || ERR) { CalcError(u * i, evalPolynomial(c, N, x) - f(x)); }
+	// calculate error
+	if (RMSE || ERR) { CalcError(u * i, evalPolynomial(P, N, x) - f(x)); }
 }
 
-
-// find the polynomial of best fit c(x) such that c[k(x)] ≈ f(x)
-void fitPolynomial(double(*f)(double), double(*k)(double), double x0, double x1, int dif, double *c, int N, double *RMSE, double *ERR) {
+// find the polynomial of best fit P(x) such that P[k(x)] ≈ f(x)
+void fitPolynomial(double(*f)(double), double(*k)(double), double x0, double x1, int dif, double *P, int N, double *RMSE, double *ERR) {
 	// It is best to integrate analytically if the interval contains indifferentiable point
 	int L = N + 1, n = dif * 2;
 	double u = (x1 - x0) / n, x, kx, xp;
 	double *sumxn = new double[2 * N + 1], *sumxny = new double[L];
 
 	// sumxn[i] = Integral[u(x)ⁱ,x0,x1]
-	for (int d = 0; d <= 2 * N; d++) sumxn[d] = 0;
-	for (int i = 0; i <= n; i++) {
-		x = x0 + u * i, kx = k(x), xp = i == 0 || i == n ? 1.0 : i & 1 ? 4.0 : 2.0;
-		for (int d = 0; d <= 2 * N; d++) sumxn[d] += xp, xp *= kx;
+	for (int i = 0; i <= 2 * N; i++) sumxn[i] = 0;
+	for (int d = 0; d <= n; d++) {
+		x = x0 + u * d, kx = k(x), xp = d == 0 || d == n ? 1.0 : d & 1 ? 4.0 : 2.0;
+		for (int i = 0; i <= 2 * N; i++) sumxn[i] += xp, xp *= kx;
 	}
-	for (int d = 0; d <= 2 * N; d++) sumxn[d] *= u / 3;
+	for (int i = 0; i <= 2 * N; i++) sumxn[i] *= u / 3;
 
 	// sumxny[i] = Integral[f(x)·u(x)ⁱ,x0,x1]
-	for (int d = 0; d < L; d++) sumxny[d] = 0;
-	for (int i = 0; i <= n; i++) {
-		x = x0 + u * i, kx = k(x), xp = (i == 0 || i == n ? 1.0 : i & 1 ? 4.0 : 2.0) * f(x);
-		for (int d = 0; d < L; d++) sumxny[d] += xp, xp *= kx;
+	for (int i = 0; i < L; i++) sumxny[i] = 0;
+	for (int d = 0; d <= n; d++) {
+		x = x0 + u * d, kx = k(x), xp = (d == 0 || d == n ? 1.0 : d & 1 ? 4.0 : 2.0) * f(x);
+		for (int i = 0; i < L; i++) sumxny[i] += xp, xp *= kx;
 	}
-	for (int d = 0; d < L; d++) sumxny[d] *= u / 3;
+	for (int i = 0; i < L; i++) sumxny[i] *= u / 3;
 
 	InitMat;
-	Eliminate;
+	Eliminate(Mat, P, L);
+	delete Mat;
 
-	// calculate error - spend time!
-	if (RMSE || ERR) { CalcError(x0 + u * i, evalPolynomial(c, N, k(x)) - f(x)); }
+	if (RMSE || ERR) { CalcError(x0 + u * i, evalPolynomial(P, N, k(x)) - f(x)); }
 }
 
 
@@ -189,10 +189,12 @@ int main() {
 	auto t0 = std::chrono::high_resolution_clock::now();
 	for (int N = 1; N <= 10; N++) {
 		double RMSE, ERR;
-		fitPolynomial(F, PI / 2, 100000, c, N, &RMSE, &ERR);
-		//fitPolynomial([](double x) {return cos(acos(x) / 3); }, [](double x) {return sqrt(x + 1); }, -1, 1, 1000000, c, N, &RMSE, &ERR);
-		printf("deg = %d, RMSE = %.2le, ERR = %.2le\n", N, RMSE, ERR);
-		printPolynomial(c, N, "u(x)");
+		fitPolynomial(F, 2, 100000, c, N, &RMSE, &ERR);
+		printf("deg = %d, RMSE = %.2le, ERR = %.2le\n", N, RMSE, ERR); printPolynomial(c, N);
+		fitPolynomial(F, 0, PI / 2, 100000, c, N, &RMSE, &ERR);
+		printf("deg = %d, RMSE = %.2le, ERR = %.2le\n", N, RMSE, ERR); printPolynomial(c, N);
+		fitPolynomial([](double x) {return cos(acos(x) / 3); }, [](double x) {return sqrt(x + 1); }, -1, 1, 1000000, c, N, &RMSE, &ERR);
+		printf("deg = %d, RMSE = %.2le, ERR = %.2le\n", N, RMSE, ERR); printPolynomial(c, N, "u(x)");
 	}
 	auto t1 = std::chrono::high_resolution_clock::now();
 	double time_elapsed = std::chrono::duration<double>(t1 - t0).count();
