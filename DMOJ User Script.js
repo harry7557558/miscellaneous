0c1ae52678cd2f84c09f56978f973d7368107489
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2020-05-31 10:16:38 -0400

    Update DMOJ User Script.js

--- DMOJ User Script.js
+++ DMOJ User Script.js
@@ -4,101 +4,125 @@ $(document).ready(function() {
 });
 
 // show problem type on problem page
-$(document).ready(function(){
-    if (document.URL.match("problem")){
+$(document).ready(function() {
+    if (document.URL.match("problem")) {
         document.getElementById("problem-types").childNodes[1].click();
     }
 });
 
+// get the username on the current page
+function getUsername() {
+    var url = window.location.href
+      , username = "";
+    if (url.match("https://dmoj.ca/user/"))
+        username = url.substring(21, url.length);
+    if (username.length == 0)
+        username = document.getElementById("user-links").getElementsByTagName("b")[0].innerText;
+    if (username.match("\/"))
+        username = username.substring(0, username.search("\/"));
+    return username;
+}
+
+// Customize user table
+// Addd a scale to Points and Problems in user table,
+// and add an Average Point Per Problem column.
 $(function() {
+    var table = document.getElementById("users-table");
+    if (table == null)
+        return;
 
-    // Customize user table
-    // Addd a scale to Points and Problems in user table,
-    // and add an Average Point Per Problem column.
-
-    $(function() {
-        var table = document.getElementById("users-table");
-        if (table == null)
-            return;
-
-        // calculate scale upper bound
-        var maxpnt = 0
-          , maxprb = 0
-          , maxppp = 0;
-        if (table.rows[0].cells.length > 6)
-            return;
-        for (var i = 0; i < table.rows.length; i++) {
-            if (table.rows[i].cells.length < 6)
-                table.rows[i].insertCell(table.rows[i].cells.length);
-            var k = table.rows[i].cells;
-            if (i == 0) {
-                k[3].style.minWidth = k[4].style.minWidth = k[5].style.minWidth = "80px";
-                k[5].outerHTML = k[5].outerHTML.replace(/td/g, "th");
-                k[5].innerText = "APPP";
-                k[5].title = "Average Point Per Problem";
-            } else {
-                var points = Number(k[3].title);
-                var problems = Number(k[4].innerText);
-                var ratio = points / problems;
-                if (isNaN(ratio)) {
-                    k[5].innerText = "#";
-                } else {
-                    k[5].innerText = ratio.toFixed(2);
-                    k[5].title = ratio.toFixed(3);
-                    maxpnt = Math.max(maxpnt, points);
-                    maxprb = Math.max(maxprb, problems);
-                    maxppp = Math.max(maxppp, ratio * (1 - Math.exp(0.1 * (1 - problems))));
-                }
-            }
-        }
-
-        // set table element style
-        var toString = function(r, g, b, a) {
-            return "rgba(" + Math.floor(255 * r) + "," + Math.floor(255 * g) + "," + Math.floor(255 * b) + "," + a + ")";
-        }
-          , gradientToString = function(c1, c2) {
-            return "linear-gradient(to right, " + c1 + ", " + c2 + ")";
-        }
-          , stepToString = function(c1, c2, a) {
-            var al = " " + 100 * a + "%, ";
-            return "repeating-linear-gradient(to right, " + c1 + "," + c1 + al + c2 + al + c2 + "100%)";
-        };
-        for (var i = 1; i < table.rows.length; i++) {
-            var k = table.rows[i].cells;
+    // calculate scale upper bound
+    var maxpnt = 0
+      , maxprb = 0
+      , maxppp = 0;
+    if (table.rows[0].cells.length > 6)
+        return;
+    for (var i = 0; i < table.rows.length; i++) {
+        if (table.rows[i].cells.length < 6)
+            table.rows[i].insertCell(table.rows[i].cells.length);
+        var k = table.rows[i].cells;
+        if (i == 0) {
+            k[3].style.minWidth = k[4].style.minWidth = k[5].style.minWidth = "80px";
+            k[5].outerHTML = k[5].outerHTML.replace(/td/g, "th");
+            k[5].innerText = "APPP";
+            k[5].title = "Average Point Per Problem";
+        } else {
             var points = Number(k[3].title);
             var problems = Number(k[4].innerText);
             var ratio = points / problems;
-
-            col = toString(1, 0.2, 0.2, 1.0 - Math.exp(-0.001 * points));
-            k[3].style.backgroundColor = col;
-            k[3].style.backgroundImage = stepToString(col, "rgba(0,0,0,0)", points / maxpnt);
-
-            col = toString(0.2, 1, 0.2, 1.0 - Math.exp(-0.001 * problems));
-            k[4].style.backgroundColor = col;
-            k[4].style.backgroundImage = stepToString(col, "rgba(0,0,0,0)", problems / maxprb);
-
-            col = toString(0.2, 0.2, 1, 1.0 - Math.exp(-0.08 * ratio));
-            k[5].style.backgroundColor = col;
-            k[5].style.backgroundImage = stepToString(col, "rgba(0,0,0,0)", ratio / maxppp);
+            if (isNaN(ratio)) {
+                k[5].innerText = "#";
+            } else {
+                k[5].innerText = ratio.toFixed(2);
+                k[5].title = ratio.toFixed(3);
+                maxpnt = Math.max(maxpnt, points);
+                maxprb = Math.max(maxprb, problems);
+                maxppp = Math.max(maxppp, ratio * (1 - Math.exp(0.1 * (1 - problems))));
+            }
         }
-    });
-
-    // DMOJ Point Calculator
-    // Press F1 to calculate the points your earned points after solving certain problem(s)
-
-    var getUsername = function() {
-        var url = window.location.href
-          , username = "";
-        if (url.match("https://dmoj.ca/user/"))
-            username = url.substring(21, url.length);
-        if (username.length == 0)
-            username = document.getElementById("user-links").getElementsByTagName("b")[0].innerText;
-        if (username.match("\/"))
-            username = username.substring(0, username.search("\/"));
-        return username;
     }
 
-    var pointcalc_pnts, pointcalc_ac;
+    // set table element style
+    var toString = function(r, g, b, a) {
+        return "rgba(" + Math.floor(255 * r) + "," + Math.floor(255 * g) + "," + Math.floor(255 * b) + "," + a + ")";
+    }
+      , gradientToString = function(c1, c2) {
+        return "linear-gradient(to right, " + c1 + ", " + c2 + ")";
+    }
+      , stepToString = function(c1, c2, a) {
+        var al = " " + 100 * a + "%, ";
+        return "repeating-linear-gradient(to right, " + c1 + "," + c1 + al + c2 + al + c2 + "100%)";
+    };
+    for (var i = 1; i < table.rows.length; i++) {
+        var k = table.rows[i].cells;
+        var points = Number(k[3].title);
+        var problems = Number(k[4].innerText);
+        var ratio = points / problems;
+
+        col = toString(1, 0.2, 0.2, 1.0 - Math.exp(-0.001 * points));
+        k[3].style.backgroundColor = col;
+        k[3].style.backgroundImage = stepToString(col, "rgba(0,0,0,0)", points / maxpnt);
+
+        col = toString(0.2, 1, 0.2, 1.0 - Math.exp(-0.001 * problems));
+        k[4].style.backgroundColor = col;
+        k[4].style.backgroundImage = stepToString(col, "rgba(0,0,0,0)", problems / maxprb);
+
+        col = toString(0.2, 0.2, 1, 1.0 - Math.exp(-0.08 * ratio));
+        k[5].style.backgroundColor = col;
+        k[5].style.backgroundImage = stepToString(col, "rgba(0,0,0,0)", ratio / maxppp);
+    }
+});
+
+// customize problem solved page
+$(function() {
+    if (document.URL.match("dmoj.ca/user/" && document.URL.match("/solved"))) {
+        // load all problems
+        var username = getUsername();
+        var request = new XMLHttpRequest();
+        request.open("GET", "https://dmoj.ca/user/" + username + "/solved/ajax?start=0&end=100", false);
+        request.send();
+        var str = request.responseText;
+        str = str.replace('{"results": "', '').replace('", "has_more": false}', '');
+        str = str.split("\\\"").join("\"").split("\\n").join("");
+        document.getElementById("submissions-table").innerHTML = str;
+        var ele = document.getElementById("pp-load-link-wrapper");
+        if (ele != null)
+            ele.remove();
+        return;
+        // expand all solved problems
+        var ss = document.getElementsByClassName("user-problem-group");
+        for (var i = 0; i < ss.length; i++) {
+            ss[i].getElementsByClassName("toggle")[0].click();
+        }
+    }
+});
+
+// DMOJ Point Calculator
+// Press F1 to calculate the points your earned points after solving certain problem(s)
+// Press F2 to generate a table
+$(function() {
+
+    var pointcalc_pnts, pointcalc_ac, solved_problem_list = [];
     var getPointData = function(username) {
         var request = new XMLHttpRequest();
         request.open("GET", "https://dmoj.ca/api/user/submissions/" + username);
@@ -128,7 +152,10 @@ $(function() {
                     ac |= Subs[i][2];
                 } else {
                     Points.push(pp);
-                    AC += Number(ac);
+                    if (ac) {
+                        solved_problem_list.push(prob);
+                        AC++;
+                    }
                     prob = Subs[i][0],
                     pp = Subs[i][1],
                     ac = Subs[i][2];
@@ -210,6 +237,7 @@ $(function() {
             s += "35             " + (calcPoint([35]) - p0).toFixed(2) + '\n';
             s += "40             " + (calcPoint([40]) - p0).toFixed(2) + '\n';
             s += "50             " + (calcPoint([50]) - p0).toFixed(2) + '\n';
+            alert(s);
             console.log(s);
         }
 
@@ -233,6 +261,7 @@ $(function() {
         for (var j = 0; j < blacklist.length; j++)
             if (url.match(blacklist[j]))
                 bad = true;
+        //if (s[i].firstElementChild != null && s[i].firstElementChild.tagName == "IMG") ok = false;
         if (!ok && url[0] != '/') {
             s[i].style.color = bad ? "red" : "blue";
             s[i].style.fontWeight = 600;
